635619bc27d645f388efe76a6eb13b35395e416d
VaralaxmiGanta <159216502+VaralaxmiGanta@users.noreply.github.com>
2024-09-30 15:43:52 +0530

    Create unittest_add.py

---
+++ unittest_add.py
@@ -0,0 +1,13 @@
+# test_my_program.py
+
+import unittest
+from my_program import add
+
+class TestMyProgram(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(add(1, 2), 3)
+        self.assertEqual(add(-1, 1), 0)
+
+if __name__ == '__main__':
+    unittest.main()
